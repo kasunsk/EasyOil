4578f47c155a6dd68494132a84e3f30c9a59ca54
Kasun Kariyawasam <kasun.kariyawasam@pagero.com>
2018-03-07 23:16:48 +0530

    code refactor

--- src/main/java/com/oilseller/oilbrocker/platform/scheduler/UserLoginInvalidScheduler.java
+++ src/main/java/com/oilseller/oilbrocker/platform/scheduler/UserLoginInvalidScheduler.java
@@ -1,6 +1,8 @@
 package com.oilseller.oilbrocker.platform.scheduler;
 
 import com.oilseller.oilbrocker.user.service.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserLoginInvalidScheduler {
+
+    private static final Logger log = LoggerFactory.getLogger(UserLoginInvalidScheduler.class);
     
     private TokenService tokenService;
 
@@ -19,12 +23,14 @@ public class UserLoginInvalidScheduler {
     // per half an hone
     @Scheduled(fixedDelay = 1800000L, initialDelay = 1800000L)
     public void invalidLogin() {
+        log.info("Token invalid scheduler");
         tokenService.invalidLogin();
     }
 
     // Per 15 days
     @Scheduled(fixedDelay = 1296000000L, initialDelay = 1296000000L)
     public void removeInvalidLogin() {
+        log.info("Token removing scheduler");
         tokenService.removeInvalidToken();
     }
 }
