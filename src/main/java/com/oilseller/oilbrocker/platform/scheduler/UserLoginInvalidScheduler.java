6fe018526d2eba0827cbfc5212dd16714eebd472
Kasun Kariyawasam <kasun.kariyawasam@pagero.com>
2018-03-09 23:25:01 +0530

    code refactor

--- src/main/java/com/oilseller/oilbrocker/platform/scheduler/UserLoginInvalidScheduler.java
+++ src/main/java/com/oilseller/oilbrocker/platform/scheduler/UserLoginInvalidScheduler.java
@@ -21,14 +21,14 @@ public class UserLoginInvalidScheduler {
     }
 
     // per half an hone
-    @Scheduled(fixedDelay = 1800000, initialDelay = 1800000)
+    @Scheduled(fixedDelay = 1800000, initialDelay = 10000)
     public void invalidLogin() {
         log.info("Token invalid scheduler");
         tokenService.invalidLogin();
     }
 
     // Per 3 days
-    @Scheduled(fixedDelay = 259200000, initialDelay = 10000)
+    @Scheduled(fixedDelay = 259200000, initialDelay = 20000)
     public void removeInvalidLogin() {
         log.info("Token removing scheduler");
         tokenService.removeInvalidToken();
