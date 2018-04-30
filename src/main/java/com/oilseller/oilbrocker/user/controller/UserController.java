e64bdf5cbf7edb6a890d89536d480e6c8214f79c
Kasun Kariyawasam <kasunsk@gmail.com>
2018-04-30 20:19:12 +0530

    available user roles

--- src/main/java/com/oilseller/oilbrocker/user/controller/UserController.java
+++ src/main/java/com/oilseller/oilbrocker/user/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.oilseller.oilbrocker.user.controller;
 
 import com.oilseller.oilbrocker.user.adaptor.UserParamAdaptor;
+import com.oilseller.oilbrocker.user.dto.UserRoleType;
 import com.oilseller.oilbrocker.user.param.UserParam;
 import com.oilseller.oilbrocker.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -31,5 +33,11 @@ public class UserController {
     @RequestMapping(method = RequestMethod.GET)
     public List<UserParam> loadAllUsers() {
         return userParamAdaptor.fromDtoList(userService.loadAllUsers());
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/availableRoles", method = RequestMethod.GET)
+    public List<UserRoleType> loadAvailableRoles() {
+        return Arrays.asList(UserRoleType.values());
     }
 }
